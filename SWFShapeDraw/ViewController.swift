809ec682efd5411919c0ff959b05044c73a4d539
larryhou <larryhou@foxmail.com>
2015-08-25 15:49:20 +0800

    Clean up

--- SWFShapeDraw/ViewController.swift
+++ SWFShapeDraw/ViewController.swift
@@ -9,55 +9,6 @@
 import UIKit
 import Foundation
 
-class TestView:UIView
-{
-    override func drawRect(rect: CGRect)
-    {
-        let start  = CGPoint(x: frame.width / 2, y: 050)
-        let center = CGPoint(x: frame.width / 2, y: 200)
-        
-        let arc = CGPathCreateMutable()
-        CGPathMoveToPoint(arc, nil, start.x, start.y)
-        CGPathAddArc(arc, nil, center.x, center.y, center.y - start.y, -CGFloat(M_PI) / 2.0, CGFloat(M_PI), false)
-        
-        let lineWidth:CGFloat = 40.0
-        let strokedArc = CGPathCreateCopyByStrokingPath(arc, nil, lineWidth, CGLineCap.Butt, CGLineJoin.Miter, 10)
-        
-        let context = UIGraphicsGetCurrentContext()
-        
-        CGContextAddPath(context, strokedArc)
-        CGContextSetFillColorWithColor(context, UIColor.lightGrayColor().CGColor)
-        CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
-        CGContextDrawPath(context, CGPathDrawingMode.FillStroke)
-        
-        let shadowColor = UIColor(white: 0, alpha: 0.75).CGColor
-        CGContextSaveGState(context)
-        
-        CGContextAddPath(context, strokedArc)
-        CGContextSetShadowWithColor(context, CGSizeMake(0, 2), 3, shadowColor)
-        CGContextFillPath(context)
-        
-        CGContextRestoreGState(context)
-        
-        let colors:[CGFloat] = [0.75, 1.0,
-                                0.90, 1.0]
-        let space = CGColorSpaceCreateDeviceGray()
-        let gradient = CGGradientCreateWithColorComponents(space, colors, nil, 2)
-        
-        CGContextSaveGState(context)
-        
-        CGContextAddPath(context, strokedArc)
-        CGContextClip(context)
-        
-        let bounds = CGPathGetBoundingBox(strokedArc)
-        let gradientStart = CGPointMake(0, CGRectGetMinY(bounds))
-        let gradientEnd = CGPointMake(0, CGRectGetMaxY(bounds))
-        
-        CGContextDrawLinearGradient(context, gradient, gradientStart, gradientEnd, CGGradientDrawingOptions.DrawsBeforeStartLocation)
-        CGContextRestoreGState(context)
-    }
-}
-
 class ViewController: UIViewController
 {
     private var shape:VectorShapeView!
@@ -83,16 +34,7 @@ class ViewController: UIViewController
             }
         }
         
-        let testView = TestView(frame: view.frame)
-        testView.backgroundColor = UIColor.clearColor()
-//        view.addSubview(testView)
-        
-        var frame = view.frame
-        frame.origin.x = frame.width / 2
-        frame.origin.y = frame.height / 2
-        frame = view.frame
-        
-        shape = VectorShapeView(frame:frame);
+        shape = VectorShapeView(frame:view.frame);
         shape.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
         view.addSubview(shape)
         
